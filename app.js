9c0985d5b40d4a146022bb9181c00f17c521c997
jbritoc17 <jbritoc17@outlook.es>
2023-06-21 19:55:23 -0500

    trabajando en el login

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static("public"))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
 app.listen(3000, () => {
