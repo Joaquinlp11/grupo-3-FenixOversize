54496923ba0c6ec1ee2ed9428d2e5300dbc47604
jbritoc17 <jbritoc17@outlook.es>
2023-06-28 22:22:13 -0500

    agregando view register

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'));
 });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
 
 app.listen(3000, () => {
     console.log('servidor funcionando http://localhost:3000/');
