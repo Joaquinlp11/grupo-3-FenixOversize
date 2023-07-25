505f7aa2c98ceefadf0e90b44c03a9a729c251f2
Joaquinlp11 <joaquinlp58@gmail.com>
2023-07-25 01:30:35 -0300

    CreateProduct

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -4,7 +4,20 @@ const productsControllers= {
 
     getDetail: (req, res)=>{
         res.render('detail')
+    },
+
+    getSelectCategory: (req, res) => {
+        res.render('selectCategory')
+    },
+
+    getNewProductC: (req, res) => {
+        res.render('createProductC')
+    },
+
+    getNewProductV: (req, res) => {
+        res.render('createProductV')
     }
+
 }
 
 module.exports = productsControllers
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -3,9 +3,12 @@ const productsControllers = require('../controllers/productsControllers')
 const router = express.Router();
 
 //@get /l
-router.get ( '/detail' , productsControllers.getDetail );
-router.get ( '/')
+router.get ('/detail', productsControllers.getDetail);
 
+router.get ('/newproductc', productsControllers.getNewProductC);
 
+router.get ('/selectcategory', productsControllers.getSelectCategory);
+
+router.get ('/newproductv', productsControllers.getNewProductV);
 
 module.exports = router;
